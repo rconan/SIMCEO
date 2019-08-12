7cc5376a1eba9f5f4c75ee959659c32320ae9612
rconan <rconan@gmto.org>
2019-08-11 22:49:02 -0700

    INITIAL SETUP.PY COMMIT

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    author = 'R. Conan',
+    author_email = 'rconan@gmto.org',
+    url ='https://github.com/rconan/SIMCEO/tree/python-client',
+    name='DOS',
+    version='0.1dev',
+    packages=['dos',],
+    long_description=open('README').read(),
+)
