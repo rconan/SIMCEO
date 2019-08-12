0c04d05beb1c4ad1b8a9ae547055b2b2eb9d91f7
rconan <rconan@gmto.org>
2019-08-11 22:52:48 -0700

    MINOR FIXES TO SETUP.PY

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(
     author_email = 'rconan@gmto.org',
     url ='https://github.com/rconan/SIMCEO/tree/python-client',
     name='DOS',
+    description='GMT Dynamic Optical Simulation Client/Server application',
     version='0.1dev',
     packages=['dos',],
-    long_description=open('README').read(),
+    long_description=open('README.md').read(),
 )
