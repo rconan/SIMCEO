efca6e772457cb0c7b0d1203ee0a2a15bae1ea1e
Rodrigo Alvite Romano <rovite@gmail.com>
2019-09-04 18:06:01 -0700

    Reference input (xr) was removed from the control law. Notebook updates to be compatible with the new MPC class version.

--- dos/mpc_driver.py
+++ dos/mpc_driver.py
@@ -11,10 +11,8 @@ class MPC:
         self.logger.setLevel(verbose)
         self.__u = np.zeros(0)
         self.__xpast = np.zeros(0)
-        self.xr = np.zeros(0)
         self.npred = npred
 
-#    def init(self,A,B,Q,R,npred,dumin,dumax,umin,umax):
         self.logger.debug('Initializing!')
 
         # Plant model dimensions
@@ -47,7 +45,6 @@ class MPC:
         # Remaining column blocks    
         for k in range(1,npred):
             kGammarowInit, kcolInit, kcolEnd = k*self.nx, k*self.nu, (k+1)*self.nu
-            # print(k, '-', kGammarowInit, '-', kcolInit, kcolEnd)
             Gamma[:, kcolInit:kcolEnd] = np.concatenate(
                 (np.zeros((kGammarowInit,self.nu)), Gamma[kGammarowInit:,0:self.nu]), axis=0)
 
@@ -73,9 +70,14 @@ class MPC:
         # Create an OSQP object as global
         self.qpp = osqp.OSQP()
         # Setup QP problem
+        if self.logger.getEffectiveLevel() > 20:
+            verboseFlag = True #Verbose mode just from INFO level (or beyond)
+        else:
+            verboseFlag = False
+
         self.qpp.setup(P, np.zeros(npred*self.nx), Ain,
             np.zeros(Ain.shape[0]), np.zeros(Ain.shape[0]),
-            verbose=True, warm_start=True)
+            verbose=verboseFlag, warm_start=True)
         # MPC-QP data matrices
         self.mpc_qpdt = {
             'Phi':Phi, 'GammaT':Gamma.T, 'Qkron':Qkron.toarray(),
@@ -85,31 +87,30 @@ class MPC:
     def init(self):
         self.__u = np.zeros(self.nu)
         self.__xpast = np.zeros(self.nx)
-        self.xr = np.zeros(self.nx*self.npred)
 
     def update(self,x):
 
         x = x.ravel()
-        print("SHAPES: x[{0}], x_past[{1}]".format(x.shape,self.__xpast.shape))
+        
         # State feedback - update MPC internal variables
         xa = np.concatenate((x-self.__xpast,self.__xpast), axis=0)
-        self.__xpast = x   # Update past state
 
         # QP linear term - demands state feedback
         q = np.dot(np.dot(self.mpc_qpdt['GammaT'],self.mpc_qpdt['Qkron']),
-                (np.dot(self.mpc_qpdt['Phi'],xa) - self.xr))
+                (np.dot(self.mpc_qpdt['Phi'],xa)))
         # QP input constraints
         lb = np.concatenate((self.mpc_qpdt['Dumin'],
                 self.mpc_qpdt['Umin']-self.mpc_qpdt['c'].dot(self.__u)), axis=0)
         ub = np.concatenate((self.mpc_qpdt['Dumax'],
                 self.mpc_qpdt['Umax']-self.mpc_qpdt['c'].dot(self.__u)), axis=0)
         # Update QP variables
-        self.qpp.update(q, lb, ub)    
+        self.qpp.update(q, lb, ub)
+        self.__xpast = x   # Update past state  
         # Solve QP
         U = self.qpp.solve()
         # Check solver status
         if U.info.status != 'solved':
-            self.logger.debug('Infeasible QP problem!!!')
+            self.logger.error('Infeasible QP problem!!!')
         self.__u = self.__u + U.x[:self.nu]
         self.logger.debug('u: %s',self.__u)
 
