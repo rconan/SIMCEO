bb2feaea49a5b0ff9a16f5e762946335bd663d10
Rodrigo Alvite Romano <rovite@gmail.com>
2019-11-09 10:00:16 -0800

    Updates in notebooks and yaml files from GSH and SH case studies.

---
+++ dos/plot_tools.py
@@ -0,0 +1,191 @@
+#import pickle
+import  numpy as np
+import matplotlib.pyplot as plt
+#from scipy import sparse
+#from scipy.linalg import block_diag
+
+def plot_science(scienceDT,m1EsDT):
+    wfe_rms = scienceDT['wfe_rms'].timeSeries
+    seg_wfe_rms = scienceDT['segment_wfe_rms'].timeSeries
+    segpiston = scienceDT['segment_piston'].timeSeries
+    tt = scienceDT['tiptilt'].timeSeries
+    segtt = scienceDT['segment_tiptilt'].timeSeries
+    pssn = scienceDT['PSSn'].timeSeries
+    ESdeltas = m1EsDT['deltas'].timeSeries
+
+    print('Final values:\n WFE:',wfe_rms[1][-1]*1.0e9,
+          '\n', seg_wfe_rms[1][-1,:]*1.0e9,
+          '\nPSSn:',pssn[1][-1], 
+          '\nsum of abs seg piston:',np.sum(np.abs(segpiston[1][-1]))*1.0e6,
+          '\n', segpiston[1][-1,:]*1.0e6)
+
+    plt.figure(figsize=(18,10))
+    plt.subplot(521)
+    plt.plot(wfe_rms[0],wfe_rms[1]*1.0e9,'x--')
+    plt.grid(True)
+    plt.ylabel('WFE RMS')
+
+    plt.subplot(522)
+    plt.plot(wfe_rms[0],seg_wfe_rms[1]*1.0e9,'x--')
+    plt.grid(True)
+    plt.ylabel('WFE RMS')
+
+    plt.subplot(523)
+    plt.plot(pssn[0],pssn[1],'x--')
+    plt.grid(True)
+    plt.ylabel('PSSn')
+
+    plt.subplot(525)
+    plt.plot(segpiston[0],np.sum(np.abs(segpiston[1]),axis=1)*1.0e6,'x--')
+    plt.grid(True)
+    plt.ylabel('sum of abs Seg piston')
+
+    plt.subplot(526)
+    plt.plot(segpiston[0],segpiston[1]*1.0e6,'x--')
+    plt.grid(True)
+    plt.ylabel('Segment piston') 
+
+    plt.subplot(527)
+    plt.plot(segpiston[0],np.sum(np.abs(ESdeltas[1]),axis=1)*1.0e6,'x--')
+    plt.grid(True)
+    plt.ylabel('sum of abs ES deltas') 
+
+    plt.subplot(528)
+    plt.plot(segpiston[0],ESdeltas[1],'x--')
+    plt.grid(True)
+    plt.ylabel('ES deltas') 
+
+    plt.subplot(529)
+    plt.plot(tt[0],tt[1],'x--')
+    plt.grid(True)
+    plt.ylabel('TT') 
+
+    plt.subplot(5,2,10)
+    plt.plot(segtt[0],segtt[1],'x--')
+    plt.grid(True)
+    plt.ylabel('Seg TT')
+
+
+def plot_X0loadComp(m1_x0_dt, m2_x0_dt, controllerDT, show_delta, colors, markers):
+    M1Txyz = controllerDT['M1 Txyz'].timeSeries
+    M1Rxyz = controllerDT['M1 Rxyz'].timeSeries
+    M2Txyz = controllerDT['M2 Txyz'].timeSeries
+    M2Rxyz = controllerDT['M2 Rxyz'].timeSeries
+    M1BM = controllerDT['M1 BM'].timeSeries
+
+    deltaM1Txyz, deltaM1Rxyz = 1.0*np.zeros_like(M1Txyz[1]), 1.0*np.zeros_like(M1Rxyz[1])
+    deltaM2Txyz, deltaM2Rxyz = 1.0*np.zeros_like(M2Txyz[1]), 1.0*np.zeros_like(M2Rxyz[1])
+    deltaBM = 1.0*np.zeros_like(M1BM[1])
+
+    aux_plt = M1Txyz[0][-1] + 4
+    Txyz_scale = 1.0e6 # micro meters
+    asec2rad = 4.84814e-6
+    
+    plt.figure(figsize=(16,10))
+    plt.subplot(221)
+    for kmode in range(3):
+        for kseg in range(7):            
+            deltaM1Txyz[kseg,kmode,:] = M1Txyz[1][kseg,kmode,:]
+            if(show_delta):
+                deltaM1Txyz[kseg,kmode,:] = deltaM1Txyz[kseg,kmode,:] + np.array(m1_x0_dt['state']['Txyz'])[kseg,kmode]
+            else:
+                plt.plot(aux_plt, -np.array(m1_x0_dt['state']['Txyz'])[kseg,kmode]*Txyz_scale,'-',
+                    color=colors[kseg], marker=markers[kmode])    
+            plt.plot(deltaM1Txyz[kseg,kmode,:]*Txyz_scale,'--', color=colors[kseg], marker=markers[kmode])
+    plt.grid(True)
+    plt.ylabel('M1 Txyz')
+    
+    plt.subplot(222)
+    for kmode in range(3):
+        for kseg in range(7):
+            deltaM1Rxyz[kseg,kmode,:] = M1Rxyz[1][kseg,kmode,:]
+            if(show_delta):
+                deltaM1Rxyz[kseg,kmode,:] = deltaM1Rxyz[kseg,kmode,:] + np.array(m1_x0_dt['state']['Rxyz'])[kseg,kmode]
+            else:
+                plt.plot(aux_plt, -np.array(m1_x0_dt['state']['Rxyz'])[kseg,kmode]/asec2rad,'-',
+                    color=colors[kseg], marker=markers[kmode])    
+            plt.plot(deltaM1Rxyz[kseg,kmode,:]/asec2rad,'--', color=colors[kseg], marker=markers[kmode])
+    plt.grid(True)
+    plt.ylabel('M1 Rxyz [asec]')
+
+    plt.subplot(223)
+    for kmode in range(3):
+        for kseg in range(7):
+            deltaM2Txyz[kseg,kmode,:] = M2Txyz[1][kseg,kmode,:]
+            if(show_delta):
+                deltaM2Txyz[kseg,kmode,:] = deltaM2Txyz[kseg,kmode,:] + np.array(m2_x0_dt['state']['Txyz'])[kseg,kmode]
+            else:
+                plt.plot(aux_plt, -np.array(m2_x0_dt['state']['Txyz'])[kseg,kmode]*Txyz_scale,'-',
+                    color=colors[kseg], marker=markers[kmode])    
+            plt.plot(deltaM2Txyz[kseg,kmode,:]*Txyz_scale,'--', color=colors[kseg], marker=markers[kmode])
+    plt.grid(True)
+    plt.ylabel('M2 Txyz')
+
+    plt.subplot(224)
+    for kmode in range(3):
+        for kseg in range(7):
+            deltaM2Rxyz[kseg,kmode,:] = M2Rxyz[1][kseg,kmode,:]
+            if(show_delta):
+                deltaM2Rxyz[kseg,kmode,:] = deltaM2Rxyz[kseg,kmode,:] + np.array(m2_x0_dt['state']['Rxyz'])[kseg,kmode]
+            else:
+                plt.plot(aux_plt, -np.array(m2_x0_dt['state']['Rxyz'])[kseg,kmode]/asec2rad,'-',
+                    color=colors[kseg], marker=markers[kmode])    
+            plt.plot(deltaM2Rxyz[kseg,kmode,:]/asec2rad,'--', color=colors[kseg], marker=markers[kmode])
+    plt.grid(True)
+    plt.ylabel('M2 Rxyz [asec]]')
+    plt.show()
+
+    plt.figure(figsize=(16,5))
+    for kmode in range(len(M1BM[1][0,:,0])):
+        for kseg in range(7):
+            deltaBM[kseg,kmode,:] = M1BM[1][kseg,kmode,:]
+            if(show_delta):
+                deltaBM[kseg,kmode,:] = deltaBM[kseg,kmode,:] + np.array(m1_x0_dt['state']['modes'])[kseg,kmode]
+            else:
+                plt.plot(aux_plt, -np.array(m1_x0_dt['state']['modes'])[kseg,kmode],'-',
+                    color=colors[kseg], marker=markers[kmode])    
+            plt.plot(deltaBM[kseg,kmode,:],'--', color=colors[kseg])
+    plt.grid(True)
+    plt.ylabel('u: M1 BM cmd')
+
+
+def plot_states(X_timeseries, n_bm):
+
+    isample = X_timeseries[0]
+    # Indices for M1 states
+    indX = np.reshape(np.arange(X_timeseries[1].shape[1]), [7, 6 + 6 + n_bm])
+    i1,i2,i3,i4,i5 = np.split(indX,[3, 6, 9, 12], axis=1)
+
+    xM1_Txyz = X_timeseries[1][:,np.reshape(i1,[21])]
+    xM1_Rxyz = X_timeseries[1][:,np.reshape(i2,[21])]
+    xM2_Txyz = X_timeseries[1][:,np.reshape(i3,[21])]
+    xM2_Rxyz = X_timeseries[1][:,np.reshape(i4,[21])]
+    xM1_BM = X_timeseries[1][:,np.reshape(i5,[46*7])]
+
+    plt.figure(figsize=(16,10))
+    plt.subplot(221)
+    plt.plot(isample,xM1_Txyz*1.0e6,'x--')
+    plt.grid(True)
+    plt.ylabel('M1 Txyz states')
+
+    plt.subplot(222)
+    plt.plot(isample,xM1_Rxyz,'x--')
+    plt.grid(True)
+    plt.ylabel('M1 Rxyz states')    
+
+    plt.subplot(223)
+    plt.plot(isample,xM2_Txyz*1.0e6,'x--')
+    plt.grid(True)
+    plt.ylabel('M2 Txyz states')
+
+    plt.subplot(224)
+    plt.plot(isample,xM2_Rxyz,'x--')
+    plt.grid(True)
+    plt.ylabel('M2 Rxyz states')   
+    plt.show()
+
+    plt.figure(figsize=(16,5))
+    plt.plot(isample,xM1_BM,'x--')
+    plt.grid(True)
+    plt.ylabel('M1 BM states')
+    plt.show()    
--- dos/tools.py
+++ dos/tools.py
@@ -1,6 +1,7 @@
 import pickle
 import  numpy as np
 from scipy import sparse
+from scipy.linalg import block_diag
 
 def linear_estimator_2_dos(estimator,dos_path, B_or_D='D'):
     """ Convert a linear estimator to a SIMCEO parameter file
@@ -41,8 +42,9 @@ def get_SHWFS_D(Din, includeM1S7Rz_col=True):
     n_bm = Din[0].shape[1]-12
     # Number of M1/2-RBM DoF (M2 S7-Rz is not considered)
     n_M1RBM, n_M2RBM = 41, 41
-    if(includeM1S7Rz_col):
-        n_M1RBM = 42
+    
+    #if(includeM1S7Rz_col):
+    #    n_M1RBM = 42
 
     # Interaction matrix dimension: SH-WFS valid measurements x DoF
     n_sh, n_x = sum(Dseg.shape[0] for Dseg in Din), n_M1RBM + n_M2RBM + 7*n_bm
@@ -51,7 +53,7 @@ def get_SHWFS_D(Din, includeM1S7Rz_col=True):
     Dcolsel = np.hstack([np.arange(6), n_M1RBM+np.arange(6)])
     # Initialize variables
     D, init_row = np.zeros((n_sh,n_x)), 0
-    print('WFS-SH Interaction matrix is',D.shape[0],'x',D.shape[1])
+    print('Consolidated WFS-SH Interaction matrix is',D.shape[0],'x',D.shape[1])
     # Fill interaction matrix for outer segments
     for k in range(6):
         coli = np.hstack(( Dcolsel+(6*k), n_M1RBM+n_M2RBM+(n_bm*k)+np.arange(n_bm) ))
@@ -65,61 +67,94 @@ def get_SHWFS_D(Din, includeM1S7Rz_col=True):
     return D
 
 def merge_SH_ES_D(Dsh, De, alphaBM=1, alphaEs=1):
-    """ Merge SH-WFS (Dsh) and edge sensor (De) interaction matrices. The merged
-    matrix is used to build a reconstructor using both sources of information
+    """ Merge SH-WFS (Dsh) and M1 edge sensor (De) interaction matrices. The merged
+    matrix is used to build a reconstructor using both sources of information. To merge
+    the matrices, a column relative to M1S7-Rz is introduced.
     """
     
     # Compute consolidated interation matrix if it is segment-wise
     if len(Dsh) == 7:
-        Dsh = get_SHWFS_D(Dsh, includeM1S7Rz_col=True)
-    # Number of M1&2 RBM (M2-S7Rz is not considered)
-    n_M12RBM = 83    
+        Dsh = get_SHWFS_D(Dsh)
+    # Number of M1&2 RBM (M1&M2 S7-Rz are not considered)
+    n_RBM = 41
     # Merged interaction matrix
     Da = np.vstack([
-            np.hstack([Dsh[:,:n_M12RBM], alphaBM*Dsh[:,n_M12RBM:]]),
-            np.hstack([alphaEs*De, np.zeros((De.shape[0],Dsh.shape[1]-De.shape[1]))])
+            # SH block
+            np.hstack([Dsh[:,:n_RBM],           # M1 RBM (wo S7 Rz)
+                Dsh[:,n_RBM:2*n_RBM],           # M2 RBM
+                alphaBM*Dsh[:,2*n_RBM:]]),      # M1 bending modes
+            # Edge sensor block    
+            np.hstack([alphaEs*De, 
+                np.zeros((De.shape[0], Dsh.shape[1]-De.shape[1]))])
         ])
 
     return Da
 
-def build_TSVD_RecM(D, n_r=0):
-    """ Build a reconstructor matrix (M_TSVD) as a sort of pseudo-inverse 
+def build_TSVD_RecM(D, n_r=0, insM1M2S7Rz=True):
+    """ Build a reconstructor matrix (M) as a sort of pseudo-inverse 
     of the interaction matrix (D). The procedure filters out the contribution
     of the n_r weakest singular values.
     """
-    
     U,sigma,V = np.linalg.svd(D, full_matrices=False)
-
-    # Truncated SVD
-    i_sigma = np.diag(1/sigma[:-n_r])
-    M_TSVD = np.transpose(V[:-n_r,:]).dot(i_sigma).dot(np.transpose(U[:,:-n_r]))
-    # Introduce a row for M2-S7Rz for compatibility
-    M_TSVD = np.vstack([M_TSVD[:83,:], np.zeros((1,M_TSVD.shape[1])), M_TSVD[83:,:]])
-
+    if(n_r):
+        # Truncated SVD
+        i_sigma = np.diag(1/sigma[:-n_r])
+        M = np.transpose(V[:-n_r,:]).dot(i_sigma).dot(np.transpose(U[:,:-n_r]))
+    else:
+        i_sigma = np.diag(1/sigma)
+        M = np.transpose(V).dot(i_sigma).dot(np.transpose(U))
     # Psuedo-inverse from truncated SVD
-    return M_TSVD
+
+    if(insM1M2S7Rz):
+        M = insert_M1M2_S7_Rz(M)
+
+    return M
+
+def build_CLS_RecM(Dsh, De, Pm2):
+    n_x = Dsh.shape[1]
+    n_constr = De.shape[0]+Pm2.shape[0]
+    DshTDsh = Dsh.T.dot(Dsh)
+    Gamma = np.hstack([ block_diag(De,Pm2), 
+                        np.zeros((n_constr,n_x-(De.shape[1]+Pm2.shape[1])))])
+    F = np.block([np.eye(n_x), np.zeros((n_x,n_constr))])
+    L = np.block([[DshTDsh, Gamma.T],
+                [Gamma, np.zeros((n_constr,n_constr))]])
+    iL = np.linalg.pinv(L) #,rcond=1e-8)
+    H = np.vstack([block_diag(Dsh.T,np.eye(De.shape[0])),
+            np.zeros((Pm2.shape[0],Dsh.shape[0]+De.shape[0]))])
+    M = F.dot(iL).dot(H)
+
+    return M
 
 
-def build_RLS_RecM(D, mu = 1):
+def build_RLS_RecM(Dsh, De, P, rhoP, n_r, insM1M2S7Rz=True):
     """ Function comments ...
     """
+
+    Da = merge_SH_ES_D(Dsh, De, alphaBM=1, alphaEs=1)
     
-    _U,sigma,V = np.linalg.svd(D, full_matrices=False)
-
-    # Weighting matrices
-    # q = (0.0)*np.ones_like(sigma)
-    # q[-n_r:] = sigma[-1:]/sigma[-n_r:] # 1*np.ones(nr) #
-
-    q = mu*sigma[-1]/sigma # 1*np.ones(nr) #
-    print('Regularization term coefficients:\n',q)
+    _U,sigma,V = np.linalg.svd(Da, full_matrices=False)
+    q = 0.0*np.zeros_like(sigma)
+    q[-n_r:] = sigma[-1]/sigma[-n_r:] #  np.ones(n_r) #
+    print('Regularization term coefficients:\n',q[-n_r:])
     Q = np.diag(q).dot(V)
 
-    # Damped (Regularized) least-squares
-    M_RLS = np.dot(np.linalg.inv(np.dot(D.T,D) + np.dot(Q.T,Q)), D.T)
-    # Introduce a row for M2-S7Rz for compatibility
-    M_RLS = np.vstack([M_RLS[:83,:], np.zeros((1,M_RLS.shape[1])), M_RLS[83:,:]])
-    return M_RLS
+    A = Da.T.dot(Da) + np.dot(Q.T,Q) + rhoP*np.dot(P.T,P) #
+#    UA,sigmaA,VA = np.linalg.svd(A, full_matrices=False)
+#    left_inv_A = np.transpose(VA).dot(np.diag(1/sigmaA)).dot(np.transpose(UA))
+    left_inv_A = np.linalg.pinv(A) #,rcond=1e-7)
+    M = left_inv_A.dot(Da.T)
 
+    if(insM1M2S7Rz):
+        M = insert_M1M2_S7_Rz(M)
+    
+    return M
+
+def insert_M1M2_S7_Rz(M):
+    # Introduce zero rows into M for M1M2-S7Rz for compatibility
+    return np.vstack([M[:41,:], np.zeros((1,M.shape[1])),
+                        M[41:82,:], np.zeros((1,M.shape[1])),
+                        M[82:,:]])
 
 def gen_recM_4_SIMCEO(M, wfsMask):
     """ The function performs an input transformation on the recontructor 
@@ -130,6 +165,10 @@ def gen_recM_4_SIMCEO(M, wfsMask):
 
     if len(wfsMask) != 7:
         raise Exception('SH-WFS mask is not partitioned segment-wise as expected!')
+
+    # Adjust wfs mask data dimension
+    for k in range (7):
+        wfsMask[k] = np.reshape(wfsMask[k],wfsMask[k].shape[0]*wfsMask[k].shape[1],1)
 
     # SH-WFS valid meas dimension
     n_sh = sum(np.count_nonzero(segMask) for segMask in wfsMask)
@@ -146,8 +185,11 @@ def gen_recM_4_SIMCEO(M, wfsMask):
     RecM = sparse.hstack([sparse.lil_matrix(M[:,:n_sh]).dot(Mmask),
             sparse.lil_matrix(M[:,n_sh:])]).toarray()
         
-    Msimceo = np.empty_like(RecM) 
+    Msimceo = np.empty_like(RecM)
     n_bm = (RecM.shape[0] - 84)//7
+    if((RecM.shape[0] - 84) % 7):
+        print('The number of bending modes retrieved from Dsh is not an integer.\n'
+         'Check the dimension of the reconstructor matrix!')   
 
     for k in range (7):
         row_sel = np.hstack([np.arange(6)+6*k,
