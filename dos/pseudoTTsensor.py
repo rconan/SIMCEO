aa925c18dd39dfd0c148f2bb9cf6e06944f1a523
Rodrigo Alvite Romano <rovite@gmail.com>
2020-09-03 09:12:56 -0700

    Pseudo TT sensor driver with IO delay.

--- dos/pseudoTTsensor.py
+++ dos/pseudoTTsensor.py
@@ -1,5 +1,6 @@
 import numpy as np
 import logging
+import queue
 
 logging.basicConfig()
 
@@ -11,18 +12,28 @@ class pseudoTTsensor:
         self.D_seg_tt = np.load('Telescope/linear_jitter.npz')['D_seg_tt']
         # Initialize averaging counter
         self.count = 0
+
         self.seg_tt = np.zeros((14,1))
         self.__yout = np.zeros_like(self.seg_tt)
+
+        queueSize = 12
+        #Instantiate TT queue
+        self.TTqueue = queue.Queue(queueSize)
+        #Initialize TT queue
+        for _ in range(queueSize):
+            self.TTqueue.put(np.zeros_like(self.seg_tt))
 
     def init(self):
         pass
 
     def update(self,u):
         self.logger.debug(f"u: {u.shape}")
-        # Average tip-tilt measurement
-        self.seg_tt += self.D_seg_tt@u.T
+        # Average tip-tilt delayed measurement
+        self.seg_tt += self.TTqueue.get()
         #print(f"seg_tt: {np.array_str(self.seg_tt*1e6,precision=1,suppress_small=True)}")          
         self.count += 1
+        # Save new TT sample into the 
+        self.TTqueue.put(self.D_seg_tt @ u.T)
 
     def output(self):
         #print(np.array_str(self.seg_tt/self.count ,precision=1,suppress_small=True))
